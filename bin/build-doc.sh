9ce000a6d0747edb96b6b1cdb2580a369d3c7f7b
Antoine Le Morvan <antoine@le-morvan.com>
2017-07-07 15:36:23 +0200

    Ajout du cours Mise en cluster (en cours de rédaction)

--- bin/build-doc.sh
+++ bin/build-doc.sh
@@ -14,31 +14,18 @@ DATE=$(date +%x) # La date au format Local
 for file in $FILES
 do
   echo "Debut de la compilation du fichier ${file}"
-  asciidoctor-pdf                            \
-    -a pdf-stylesdir="${SOURCES}/theme/"     \
-    -d book                                  \
-    -t                                       \
-    -n                                       \
-    -a pdf-style="asciidoctor"               \
-    -a pdf-fontsdir="${SOURCES}/theme/fonts" \
-    -a revdate="${DATE}"                     \
-    -a lang="fr"                             \
-    -a icons="font"                          \
-    -a chapter-label="Chapitre"              \
-    -a part-label="Partie "                  \
-    -a doctype="book"                        \
-    -a encoding="utf-8"                      \
-    -a toc="preamble"                        \
-    -a toc-title="Table des matières"        \
-    -a part-title="Partie "                  \
-    -a toclevels=3                           \
-    -a numbered=""                           \
-    -a docinfo=""                            \
-    -a sectnumlevels=2                       \
-    -a showtitle="titre"                     \
-    -D $DEST ${file}
+  asciidoctor-pdf \
+    -a pdf-stylesdir=${SOURCES}/theme/  \
+    -n  \
+    -a pdf-style=asciidoctor \
+    -a pdf-fontsdir=${SOURCES}/theme/fonts \
+    -a revdate=${DATE}          \
+    -a lang="fr"                \
+    -a icons="font"             \
+    -a chapter-label="Chapitre" \
+    -a part-label="Partie"      \
+    -D $DEST ${SOURCES}/${file}
   echo "Fin de la compilation du fichier ${file}"
   echo "----------------------------------------"
   echo ""
 done
-
